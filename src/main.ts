e72c17a5474f4424f3a2108718a0d706d94763e6
JeanCornelio <jcornelioweb@gmail.com>
2023-01-05 22:32:51 -0400

    App Mapas

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoiamVhbndlYiIsImEiOiJjbDhpdzU1c2cwdTIzM3dzMTJnejFtd3V3In0.1hYny__xHyyNJYGVW3K3NQ';
+
+if(!navigator.geolocation){
+  alert('Geolocalizacion no disponible');
+  throw new Error("No existe la geolocalizacion");
+}
+
+
+
 if (environment.production) {
   enableProdMode();
 }
